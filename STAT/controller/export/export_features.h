8e77eda16c4375026a39aefef82864dadb979b8b
anonymousRocketbird <info3-anongit@informatik.uni-wuerzburg.de>
2022-09-09 14:00:03 +0200

    Initial commit

---
+++ STAT/controller/export/export_features.h
@@ -0,0 +1,8 @@
+#ifndef LOW_LEVEL_LIB_EXPORT_FEATURES_H
+#define LOW_LEVEL_LIB_EXPORT_FEATURES_H
+
+#include <stdint.h>
+
+_Noreturn void *feature_export_thread(void *arg);
+
+#endif //LOW_LEVEL_LIB_EXPORT_FEATURES_H
